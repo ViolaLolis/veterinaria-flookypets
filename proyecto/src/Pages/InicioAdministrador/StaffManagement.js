e086b481282f91e6fab9fd6d73c95609d35f63c3
JuanDavid4562 <juandavidromo2020@gmail.com>
2025-05-19 17:23:57 -0500

    Modificar menu Admin

--- proyecto/src/Pages/InicioAdministrador/StaffManagement.js
+++ proyecto/src/Pages/InicioAdministrador/StaffManagement.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Styles/InicioAdministrador.css';
 import { useNavigate } from 'react-router-dom';
-import EditStaffModal from './EditStaffModal';
 import ConfirmationModal from './ConfirmationModal';
 
 const StaffManagement = () => {
